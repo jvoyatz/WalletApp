3edc583bf90ee780f2ac68f627e64b10978bf5c9
jvoyatz <jvoyatz@gmail.com>
2023-04-23 17:00:27 +0300

    importing ui module's R as ui_R

--- app/src/main/java/gr/jvoyatz/afse/wallet/WalletActivity.kt
+++ app/src/main/java/gr/jvoyatz/afse/wallet/WalletActivity.kt
@@ -3,11 +3,12 @@ package gr.jvoyatz.afse.wallet
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import gr.jvoyatz.afse.wallet.databinding.ActivityWalletBinding
+import gr.jvoyatz.afse.core.ui.R as ui_R
 
 class WalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(gr.jvoyatz.afse.core.ui.R.style.Theme_AFSEWallet)
+        setTheme(ui_R.style.Theme_AFSEWallet)
         with(ActivityWalletBinding.inflate(layoutInflater)){
             setContentView(this.root)
         }
