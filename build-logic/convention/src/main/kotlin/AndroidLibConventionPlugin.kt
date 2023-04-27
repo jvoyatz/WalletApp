4bef2f4da73e2a772850dd358b9d752d131fc7f9
jvoyatz <jvoyatz@gmail.com>
2023-04-27 22:37:03 +0300

    updated lib convention plugin

--- build-logic/convention/src/main/kotlin/AndroidLibConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibConventionPlugin.kt
@@ -7,6 +7,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
 
 /**
  * Replaces the plugin `com.android.library' in Android library modules
@@ -28,8 +29,10 @@ class AndroidLibConventionPlugin : Plugin<Project> {
 
             val libs = extensions.getVersionCatalogExtension().getLibs()
             target.dependencies {
+                "implementation"(target.project(":core:common"))
                 "implementation"(libs.findLibrary("logging.timber").get())
                 "implementation"(libs.findLibrary("coroutines").get())
+                "implementation"(libs.findLibrary("javax.inject").get())
                 "testImplementation"(libs.findBundle("test").get())
             }
         }
